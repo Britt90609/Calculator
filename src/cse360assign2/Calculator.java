d7176f3040ce42bdab0c5254f2ca60353487a926
Britt <Britt@Brittanys_Comp.3520.dhcp.asu.edu>
2019-02-18 10:43:55 -0700

    Initial commit

---
+++ src/cse360assign2/Calculator.java
@@ -0,0 +1,66 @@
+package cse360assign2;
+
+/**
+ * Author: Brittany Epstein (371)
+ * Date: 2/18/2019
+ * Description: Goal is to make a calculator. This assignment gives practice
+ * with a version control system. 
+ * Class: CSE360
+*/
+
+public class Calculator {
+
+	private int total;
+	 /**
+	  * Constructor for the calculator.
+	  */
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	/**
+	 * Gets the totals from the functions below.
+	 * 
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * Adds integers to get the total value.
+	 * @param value holds the integer to be given be returned from the calculator.
+	 */
+	public void add (int value) {
+		
+	}
+	/**
+	 * Subtracts integers to get the total value.
+	 * @param value holds the integer to be given to the calculator
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/**
+	 * Multiplies integers together to get the total value.
+	 * @param value holds the integer to be given to the calculator.
+	 */
+	public void multiply (int value) {
+		
+	}
+	
+	/**
+	 * Divides integers together to get the total value.
+	 * @param value holds the integer to be given to the calculator
+	 */
+	public void divide (int value) {
+		
+	}
+	
+	/**
+	 * Gets all the values that were returned from the functions above. 
+	 * 
+	 */
+	public String getHistory () {
+		return "";
+	}
+}
