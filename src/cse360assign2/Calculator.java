b28c6a24eee364be072ddaf005fac98f794bb983
Britt90609 <Britt90609@aol.com>
2019-02-18 14:48:17 -0700

    Initial Comments of Calculator Code.

---
+++ src/cse360assign2/Calculator.java
@@ -0,0 +1,53 @@
+package cse360assign2;
+
+public class Calculator {
+
+	private int total;
+	/**
+	 * Constructor for the calculator.
+	 */
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	/**
+	 * Gets the totals from the functions below.
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	/**
+	 * Adds integers to get the total value 
+	 * @param value holds the integer to be given be returned from the calculator.
+	 */
+	public void add (int value) {
+		
+	}
+	/**
+	 * Subtracts integers to get the total value 
+	 * @param value holds the integer to be given be returned from the calculator.
+	 */
+	public void subtract (int value) {
+		
+	}
+	/**
+	 * Multiplies integers together to get the total value 
+	 * @param value holds the integer to be given to the calculator.
+	 */
+	public void multiply (int value) {
+		
+	}
+	/**
+	 * Divides integers together to get the total value 
+	 * @param value holds the integer to be given be returned from the calculator.
+	 */
+	public void divide (int value) {
+		
+	}
+	
+	/**
+	 * This method will return a history of all actions of the functions. 
+	 */
+	public String getHistory () {
+		return "";
+	}
+}
---
+++ src/cse360assign2/Main.java
@@ -0,0 +1,17 @@
+package cse360assign2;
+
+public class Main {
+	public static void main(String[] args) {
+		Calculator testing = new Calculator();
+		
+		testing.add (4);
+		testing.subtract (2);
+		testing.multiply (2);
+		testing.add(5);
+		testing.divide(0);
+		testing.add(10);
+		testing.subtract(11);
+		testing.multiply(0);
+		
+	}
+}
